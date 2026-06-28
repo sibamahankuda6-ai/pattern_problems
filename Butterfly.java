f70bcf44c1aaf7134f467aafc4194d48721e7cce
Siba Mahankuda <sibamahankuda6@gmail.com>
2026-06-28 12:03:50 +0530

    pattern problems

---
+++ Butterfly.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Butterfly {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row:");
+        int n = sc.nextInt();
+        // upper half
+        for(int i=1;i<=n;i++){
+
+          for(int j=1;j<=i;j++){
+            System.out.print("*");
+          }
+
+          int spaces = 2*(n-i);
+          for(int j=1;j<=spaces;j++){
+            System.out.print(" ");
+          }
+
+          for(int j=1;j<=i;j++){
+            System.out.print("*");
+          }
+          System.out.println();
+        }
+
+
+
+
+        //lower part
+         for(int i=n;i>=1;i--){
+
+          for(int j=1;j<=i;j++){
+            System.out.print("*");
+          }
+          
+          int spaces = 2*(n-i);
+          for(int j=1;j<=spaces;j++){
+            System.out.print(" ");
+          }
+
+          for(int j=1;j<=i;j++){
+            System.out.print("*");
+          }
+          System.out.println();
+        }
+        sc.close();
+    }
+
+
+
+
+
+
+
+
+}
---
+++ SolidRectangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class SolidRectangle{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row:");
+        int n = sc.nextInt();
+        System.out.println("Enter column:");
+        int m = sc.nextInt();
+       
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+
+    }
+}
