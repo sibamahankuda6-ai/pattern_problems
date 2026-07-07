723afab81dd402a8cbf94f12c845569fd4cfc676
Siba Mahankuda <sibamahankuda6@gmail.com>
2026-07-07 22:35:06 +0530

    soomme spaces added

--- HalfPyramidWithNumbers.java
+++ HalfPyramidWithNumbers.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class HalfPyramidWithNumbers{
      public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
@@ -11,6 +12,8 @@ public class HalfPyramidWithNumbers{
             }
             System.out.println();
         }
+
+        
         sc.close();
 
     }
